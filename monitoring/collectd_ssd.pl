f6dc0b6bfbc615d592f8d4862ee538591cdfeacf
Mariusz Gronczewski <xani666@gmail.com>
2014-10-04 00:44:19 +0200

    add looping, probe every 5 minutes

--- monitoring/collectd_ssd.pl
+++ monitoring/collectd_ssd.pl
@@ -13,8 +13,7 @@ chomp($host);
 my $cfg = { # default config values go here
     'device-glob' => 'sd*',
     'by-model'    => 1,
-#    daemon  => 0,
-#    pidfile => 0,
+    'interval'    => 300,
 };
 my $help;
 $| = 1;
@@ -49,82 +48,97 @@ if ($help || defined( $missing_opts ) ) {
     );
 }
 
-
-my @devices = glob('/sys/block/' . $cfg->{'device-glob'});
-my $dev_status;
-foreach my $sysdev (@devices) {
-    if (! -r "$sysdev/queue/rotational") {
-        print STDERR "cant find $sysdev/queue/rotational";
-        next;
-    }
-
-    my $is_hdd = read_file("$sysdev/queue/rotational");
-    if ($is_hdd != 0) {
-        next; # not a SSD
-    }
-    my ($dev) = $sysdev =~ m{/sys/block/(.+)$};
-    my $pid = open(my $smart, '-|', '/usr/bin/sudo', '/usr/sbin/smartctl','-a',  "/dev/$dev");
-    while(<$smart>) {
-        chomp;
-        my @line = split;
-        if(!defined($line[1])) {next}
-        if($line[0] =~ /Device/ && $line[1] =~ /Model/) {
-            $dev_status->{$dev}{'model'} = $line[2];
-            $dev_status->{$dev}{'model'} =~ s{[\-/\s]}{_}g;
-        }
-        if($line[0] =~ /Serial/i && $line[1] =~ /Number/i) {
-            $dev_status->{$dev}{'serial'} = $line[2];
-            $dev_status->{$dev}{'serial'} =~ s{[\-/\s]}{_}g;
-        }
-        if($line[1] =~ /Life_Curve_Status/i) {
-            $dev_status->{$dev}{'life_curve_status'} = $line[3];
-        }
-        if($line[1] =~ /(SSD_Life_Left|Media_Wearout_Indicator)/i) {
-            $dev_status->{$dev}{'ssd_life_left'} = $line[3];
-        }
-        if($line[1] =~ /Lifetime_Writes_GiB/i) {
-            $dev_status->{$dev}{'write_gb'} = $line[9];
-        }
-        if($line[1] =~ /Lifetime_Reads_GiB/i) {
-            $dev_status->{$dev}{'read_gb'} = $line[9];
-        }
-        if($line[1] =~ /Reallocated_Event_Count/i) {
-            $dev_status->{$dev}{'reallocated'} = $line[9];
-        }
-        if($line[1] =~ /Program_Fail_Count/i) {
-            $dev_status->{$dev}{'program_fail'} = $line[9];
-        }
-        if($line[1] =~ /Erase_Fail_Count/i) {
-            $dev_status->{$dev}{'erase_fail'} = $line[9];
-        }
-        if($line[1] =~ /Retired_Block_count/i) {
-            $dev_status->{$dev}{'retired_blocks'} = $line[9];
-        }
-        if($line[1] =~ /Reported_Uncor/i) {
-            $dev_status->{$dev}{'reported_uncorrected'} = $line[9];
-        }
-
-        if($line[1] =~ /Power_Cycle_Count/i) {
-            $dev_status->{$dev}{'power_cycle'} = $line[9];
-        }
-
-        if($line[1] =~ /Unexpect_Power/i) {
-            $dev_status->{$dev}{'power_loss'} = $line[9];
-        }
-
-    }
-    close($smart);
-    waitpid($pid, 0);
+&print_ssd_status;
+while(sleep $cfg->{'interval'}) {
+    &print_ssd_status;
 }
-my $t = int(time);
-while(my ($dev, $data) = each(%$dev_status)) {
-    my $prefix = "PUTVAL $host/ssd-";
-    if ($cfg->{'by-model'}) {
-        $prefix .= $data->{'model'} . '_'  . $data->{'serial'} . '/';
-    } else {
-        $prefix .= $dev . '/';
+
+sub print_ssd_status {
+
+    my @devices = glob('/sys/block/' . $cfg->{'device-glob'});
+    my $dev_status;
+    foreach my $sysdev (@devices) {
+        if (! -r "$sysdev/queue/rotational") {
+            print STDERR "cant find $sysdev/queue/rotational";
+            next;
+        }
+
+        my $is_hdd = read_file("$sysdev/queue/rotational");
+        if ($is_hdd != 0) {
+            next; # not a SSD
+        }
+        my ($dev) = $sysdev =~ m{/sys/block/(.+)$};
+        my $pid = open(my $smart, '-|', '/usr/bin/sudo', '/usr/sbin/smartctl','-a',  "/dev/$dev");
+        while(<$smart>) {
+            chomp;
+            my @line = split;
+            if(!defined($line[1])) {next}
+            if($line[0] =~ /Device/ && $line[1] =~ /Model/) {
+                $dev_status->{$dev}{'model'} = $line[2];
+                $dev_status->{$dev}{'model'} =~ s{[\-/\s]}{_}g;
+            }
+            if($line[0] =~ /Serial/i && $line[1] =~ /Number/i) {
+                $dev_status->{$dev}{'serial'} = $line[2];
+                $dev_status->{$dev}{'serial'} =~ s{[\-/\s]}{_}g;
+            }
+            if($line[1] =~ /Life_Curve_Status/i) {
+                $dev_status->{$dev}{'life_curve_status'} = $line[3] + 0;
+            }
+            if($line[1] =~ /(SSD_Life_Left|Media_Wearout_Indicator|Wear_Leveling_Count)/i) {
+                $dev_status->{$dev}{'ssd_life_left'} = $line[3] + 0;
+            }
+            if($line[1] =~ /(Wear_Leveling_Count)/i && $line[9] > 0) {
+                $dev_status->{$dev}{'wear_leveling_count'} = $line[9]+0;
+            }
+
+            if($line[1] =~ /Lifetime_Writes_GiB/i) {
+                $dev_status->{$dev}{'write_gb'} = $line[9];
+            }
+            if($line[1] =~ /Lifetime_Reads_GiB/i) {
+                $dev_status->{$dev}{'read_gb'} = $line[9];
+            }
+            if($line[1] =~ /Reallocated_(Event_Count|Sector_Ct)/i) {
+                $dev_status->{$dev}{'reallocated'} = $line[9];
+            }
+            if($line[1] =~ /Program_Fail_(Cnt|Count)/i) {
+                $dev_status->{$dev}{'program_fail'} = $line[9];
+            }
+            if($line[1] =~ /Erase_Fail_(Cnt|Count)/i) {
+                $dev_status->{$dev}{'erase_fail'} = $line[9];
+            }
+            if($line[1] =~ /Retired_Block_count/i) {
+                $dev_status->{$dev}{'retired_blocks'} = $line[9];
+            }
+            if($line[1] =~ /Reported_Uncor/i) {
+                $dev_status->{$dev}{'reported_uncorrected'} = $line[9];
+            }
+
+            if($line[1] =~ /Power_Cycle_Count/i) {
+                $dev_status->{$dev}{'power_cycle'} = $line[9];
+            }
+
+            if($line[1] =~ /Unexpect_Power/i) {
+                $dev_status->{$dev}{'power_loss'} = $line[9];
+            }
+
+            if($line[1] =~ /Total_LBAs_Written/i) {
+                $dev_status->{$dev}{'lba_written'} = $line[9];
+            }
+
+        }
+        close($smart);
+        waitpid($pid, 0);
     }
-    for my $var (grep {!/(serial|model)/} keys %$data)   {
-        print $prefix . "gauge-$var" . ' interval=60 ' . "$t:" . $data->{$var} . "\n";
+    my $t = int(time);
+    while(my ($dev, $data) = each(%$dev_status)) {
+        my $prefix = "PUTVAL $host/ssd-";
+        if ($cfg->{'by-model'}) {
+            $prefix .= $data->{'model'} . '_'  . $data->{'serial'} . '/';
+        } else {
+            $prefix .= $dev . '/';
+        }
+        for my $var (grep {!/(serial|model)/} keys %$data)   {
+            print $prefix . "gauge-$var" . " interval=$cfg->{'interval'} " . "$t:" . $data->{$var} . "\n";
+        }
     }
 }
