87798fb3ecd9ed6d403a694b49598f9482fe6d24
Mariusz Gronczewski <xani666@gmail.com>
2018-09-11 14:38:27 +0200

    test example

--- project-templates/go-chi/main_test.go
+++ project-templates/go-chi/main_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 var testStrings []string
@@ -21,24 +22,25 @@ func TestExample(t *testing.T) {
 	// pass 'nil' as the third parameter.
 	//req, err := http.NewRequest("GET", "/health-check", nil)
 	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 	// directly and pass in our Request and ResponseRecorder.
 	router.ServeHTTP(rr, req)
+	Convey("/ returns 200", t, func() {
+		So(err,ShouldBeNil)
+		So(rr.Code, ShouldEqual, http.StatusOK)
+		So(rr.Body.String(), ShouldContainSubstring, "welcome")
+	})
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+	req2, err := http.NewRequest("GET", "/p/testparam", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
-
-	// Check the response body is what we expect.
-	expected := `welcome`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
+	rr2 := httptest.NewRecorder()
+	router.ServeHTTP(rr2, req2)
+		Convey("/p/:param returns param", t, func() {
+		So(err,ShouldBeNil)
+		So(rr2.Code, ShouldEqual, http.StatusOK)
+		So(rr2.Body.String(), ShouldContainSubstring, "testparam")
+	})
 }
