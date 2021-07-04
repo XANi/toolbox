c56cd3e79e213bec5642b63b8ba75f0623bc63b7
Mariusz Gronczewski <xani666@gmail.com>
2021-07-04 23:55:03 +0200

    blinky

--- project-templates/rust-arm/src/main.rs
+++ project-templates/rust-arm/src/main.rs
@@ -1,20 +1,60 @@
+
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
-use cortex_m::asm;
+use nb::block;
+
+use stm32f1xx_hal::{
+    prelude::*,
+    pac,
+    timer::Timer,
+};
+//use cortex_m_semihosting::hprintln;
 use cortex_m_rt::entry;
+use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    // Access to peripherals common to Cortex M CPUs
+    let core_peripherals = cortex_m::Peripherals::take().unwrap();
+    // Access to peripherals that are device specific
+    let device_peripherals = pac::Peripherals::take().unwrap();
 
+    // RCC is responsible for reset and clock signal configuration
+    // https://wiki.st.com/stm32mpu/wiki/RCC_internal_peripheral
+    let mut rcc = device_peripherals.RCC.constrain();
+
+    // FLASH is responsible for flash config (timing etc.)
+    let mut flash = device_peripherals.FLASH.constrain();
+
+    // freeze the RCC config. This is used so other functions have a base
+    // to calculate say interrupt interval on timer
+    // this is basically "continue working on internal RC clock that you used on boot"
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // Get the GPIOC peripheral
+    let mut gpioc = device_peripherals.GPIOC.split(&mut rcc.apb2);
+
+    // Extract the PC13 (on Blue Pill that's a pin with LED)
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    // Initialize timer based on the system clock, and calculate how much it would need to run with 6 Hz
+    // then start running
+    let mut timer = Timer::syst(core_peripherals.SYST, &clocks).start_count_down(6.hz());
+
+    // we're not using interrupts here, just busy wait for simplicity
     loop {
-        // your code goes here
+        // wait for our timer to "tick"
+        block!(timer.wait()).unwrap();
+        // set a led
+        led.set_high().unwrap();
+        // wait again
+        block!(timer.wait()).unwrap();
+        // unset a led
+        led.set_low().unwrap();
+        // we're not using XOR because that requires read-modify-write loop but just setting/resetting value have hardware optimization on STM32
     }
 }
