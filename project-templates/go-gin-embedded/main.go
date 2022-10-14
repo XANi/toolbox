cb13577ab8f6db86890a96e33e50e518af10017a
Mariusz Gronczewski <xani666@gmail.com>
2022-10-15 00:05:51 +0200

    pprof, auto-detect local files

--- project-templates/go-gin-embedded/main.go
+++ project-templates/go-gin-embedded/main.go
@@ -7,6 +7,9 @@ import (
 	"github.com/urfave/cli"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"io/fs"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 )
 
@@ -16,7 +19,7 @@ var debug = true
 
 // /* embeds with all files, just dir/ ignores files starting with _ or .
 //go:embed static templates
-var webContent embed.FS
+var embeddedWebContent embed.FS
 
 func init() {
 	consoleEncoderConfig := zap.NewDevelopmentEncoderConfig()
@@ -63,7 +66,7 @@ func main() {
 	app.Description = "do foo to bar"
 	app.Version = version
 	app.HideHelp = true
-	log.Errorf("Starting %s version: %s", app.Name, version)
+	log.Infof("Starting %s version: %s", app.Name, version)
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{Name: "help, h", Usage: "show help"},
 		cli.BoolFlag{Name: "debug, d", Usage: "enable debug logs"},
@@ -73,6 +76,11 @@ func main() {
 			Usage:  "Listen addr",
 			EnvVar: "LISTEN_ADDR",
 		},
+		cli.StringFlag{
+			Name:  "pprof-addr",
+			Value: "",
+			Usage: "address to run pprof on, disabled by default",
+		},
 	}
 	app.Action = func(c *cli.Context) error {
 		if c.Bool("help") {
@@ -81,12 +89,28 @@ func main() {
 		}
 		debug = c.Bool("debug")
 		log.Debug("debug enabled")
+		var webDir fs.FS
+		webDir = embeddedWebContent
+		if st, err := os.Stat("./static"); err == nil && st.IsDir() {
+			if st, err := os.Stat("./templates"); err == nil && st.IsDir() {
+				webDir = os.DirFS(".")
+				log.Infof(`detected directories "static" and "templates", using local static files instead of ones embedded in binary`)
+			}
+		}
+
+		os.DirFS(".")
 		w, err := web.New(web.Config{
 			Logger:     log,
 			ListenAddr: c.String("listen-addr"),
-		}, webContent)
+		}, webDir)
 		if err != nil {
 			log.Panicf("error starting web listener: %s", err)
+		}
+		if len(c.String("pprof-addr")) > 0 {
+			log.Infof("listening pprof on %s", c.String("pprof-addr"))
+			go func() {
+				log.Errorf("failed to start debug listener: %s (ignoring)", http.ListenAndServe(c.String("pprof-addr"), nil))
+			}()
 		}
 		return w.Run()
 	}
