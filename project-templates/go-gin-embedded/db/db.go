4a245f60763c52b137abb2ae30432e8bc1d92af5
Mariusz Gronczewski <xani666@gmail.com>
2022-10-16 17:05:22 +0200

    add db stub

---
+++ project-templates/go-gin-embedded/db/db.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	DSN    string
+	DbType string
+	Logger *zap.SugaredLogger
+}
+
+type DB struct {
+	d   *gorm.DB
+	l   *zap.SugaredLogger
+	cfg Config
+}
+
+func New(cfg Config) (*DB, error) {
+	var dbConn *gorm.DB
+	var err error
+	switch cfg.DbType {
+	case "sqlite":
+		dbConn, err = gorm.Open(sqlite.Open(cfg.DSN))
+	case "pgsql":
+		dbConn, err = gorm.Open(postgres.Open(cfg.DSN))
+	default:
+		return nil, fmt.Errorf("db type [%s] not supported", err)
+	}
+	if err != nil {
+		return &DB{}, err
+	}
+	var dbObj DB
+	if cfg.Logger != nil {
+		dbObj.l = cfg.Logger
+	} else {
+		dbObj.l = zap.S()
+	}
+	dbObj.d = dbConn
+	dbObj.cfg = cfg
+	//err = dbObj.d.Set("gorm:table_options", tableOptions).AutoMigrate(Record{})
+	//err = dbObj.d.AutoMigrate(Record{})
+	if err != nil {
+		return nil, err
+	}
+	return &dbObj, err
+}
--- project-templates/go-gin-embedded/go.mod
+++ project-templates/go-gin-embedded/go.mod
@@ -6,7 +6,14 @@ require (
 	github.com/efigence/go-mon v1.3.1
 	github.com/gin-contrib/zap v0.0.1
 	github.com/gin-gonic/gin v1.7.2
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.0
 	github.com/urfave/cli v1.22.5
-	go.uber.org/zap v1.17.0
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
+	golang.org/x/text v0.3.8 // indirect
+	gorm.io/driver/postgres v1.4.4
+	gorm.io/driver/sqlite v1.4.2
+	gorm.io/gorm v1.24.0
 )
